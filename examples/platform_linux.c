d927ebd5db764be669c77e5c5a4f4a97dd16cca0
Alexsey <alexsey.websites.work@gmail.com>
2026-03-01 14:17:32 +0300

    Update platform_linux.c

--- examples/platform_linux.c
+++ examples/platform_linux.c
@@ -47,6 +47,25 @@ static int linux_recv(void* ud, void* buf, size_t max_count) {
     return (r > 0) ? (int)r : ((r == 0) ? 0 : -1);
 }
 
+/* Automatic DC connection - TCP connect using mtproto_get_dc_address */
+static int linux_connect_to_dc(void* ud, int dc_id, int server) {
+    char host[64];
+    int port;
+    struct sockaddr_in addr;
+    (void)ud;
+    (void)server;
+    if (g_fd >= 0) { close(g_fd); g_fd = -1; }
+    if (!mtproto_get_dc_address(dc_id, server, host, sizeof(host), &port)) return 0;
+    g_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (g_fd < 0) return 0;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons((uint16_t)port);
+    if (inet_pton(AF_INET, host, &addr.sin_addr) <= 0) { close(g_fd); g_fd = -1; return 0; }
+    if (connect(g_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) { close(g_fd); g_fd = -1; return 0; }
+    return 1;
+}
+
 void mtproto_linux_example(void) {
     mtproto_callbacks_t cbs = {
         .get_time_ms = linux_get_time_ms,
@@ -55,8 +74,17 @@ void mtproto_linux_example(void) {
         .recv_data = linux_recv,
         .log = NULL,
         .auth_callback = NULL,
+        .connect_to_dc = linux_connect_to_dc,
         .userdata = NULL
     };
-    /* Connect g_fd to Telegram DC, then use mtproto_create/connect/send/recv */
-    (void)cbs;
+    mtproto_state_t* state = mtproto_create(&cbs);
+    mtproto_session_t* session = NULL;
+    if (state) {
+        session = mtproto_connect(state, 2, MTPROTO_SERVER_PRODUCTION);
+        if (session) {
+            /* mtproto_do_auth_handshake, send_phone, etc. */
+            mtproto_disconnect(session);
+        }
+        mtproto_destroy(state);
+    }
 }
