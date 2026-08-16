9f39ed5897fc2ca13e10157b153ba106b49f657e
Alexsey <alexsey.websites.work@gmail.com>
2026-08-16 23:56:56 +0300

    Update warning message in mtproto.h

--- mtproto.h
+++ mtproto.h
@@ -1,4 +1,4 @@
-/* WARNING: IS NOT ALL 
+/* WARNING: This library is not a full-fledged MTProto implementation
  * mtproto.h - Single-file header-only C library for MTProto protocol
  * (Telegram messaging protocol)
  *
